6732ed278006be95536871aa8695e1b6f3cbb7db
Heidi Jay <HeidiJay@yahoo.com>
2022-08-19 16:30:30 -0400

    Corrected UNION query for lines 200-214

--- recycle_code.sql
+++ recycle_code.sql
@@ -201,14 +201,14 @@ GROUP BY region;
 SELECT
     sport,
     COUNT(DISTINCT event) AS events
-FROM summer_games AS c1
+FROM summer_games
 GROUP BY sport
 UNION 
 -- Select sport and events for winter sports
 SELECT
     sport
     COUNT(DISTINCT event) AS events
-FROM winter_games AS c2
+FROM winter_games
 GROUP BY sport
 -- Show the most events at the top of the report
 ORDER BY sport DESC;
